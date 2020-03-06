077ed094311ab41463f3c9e0d4215bfafbc7647d
valevas6 <39610402+valevas6@users.noreply.github.com>
2020-03-06 16:26:38 -0600

    animationframe

---
+++ animationframe/js/animationframe.js
@@ -0,0 +1,14 @@
+var globalID;
+
+function repeatOften() {
+  $("<div />").appendTo("body");
+  globalID = requestAnimationFrame(repeatOften);
+}
+
+$("#start").on("click", function() {
+  globalID = requestAnimationFrame(repeatOften);
+});
+
+$("#stop").on("click", function() {
+  cancelAnimationFrame(globalID);
+});
